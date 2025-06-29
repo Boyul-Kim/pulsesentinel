8957438dc12e8807dadff3381711adc8163b0c1d
Boyul-Kim <boyulkim1@gmail.com>
2025-06-29 16:13:34 -0700

    parsing event outcomes - need to set up kafka

--- internal/event-ingestor/event-ingestor.go
+++ internal/event-ingestor/event-ingestor.go
@@ -1,6 +1,7 @@
 package event_ingestor
 
 import (
+	"fmt"
 	"io"
 	"log"
 
@@ -22,7 +23,13 @@ func (s *EventIngestorServer) StreamEvents(stream pb.EventIngestor_StreamEventsS
 			log.Printf("Stream error: %v", err)
 			return err
 		}
-		log.Printf("Received event: %v", event)
+		log.Printf("Received event: %v", event.Event.Outcome)
+		switch event.Event.Outcome {
+		case "success":
+			fmt.Println("Benign event logged")
+		case "failure":
+			fmt.Printf("Potential harmful event detected: %v", event)
+		}
 
 		resp := &pb.EventResponse{Message: "Received event " + event.EventId}
 		if err := stream.Send(resp); err != nil {
