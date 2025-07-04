f5dba2726ee6e4855a583e6a48901c40ff4fc270
Boyul-Kim <boyulkim1@gmail.com>
2025-07-04 15:33:26 -0700

    working on kafka consumer

--- cmd/monitor/main.go
+++ cmd/monitor/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"github.com/Boyul-Kim/pulsesentinel/internal/monitor"
+	"github.com/segmentio/kafka-go"
+)
 
 func main() {
-	fmt.Println("monitor")
+	r := kafka.NewReader(kafka.ReaderConfig{
+		Brokers:  []string{"localhost:9092"},
+		Topic:    "alerts",
+		GroupID:  "monitoring",
+		MinBytes: 10e3, // 10KB
+		MaxBytes: 10e6, // 10MB
+	})
+
+	monitor := &monitor.MonitorServer{KafkaConsumer: r}
+	monitor.ConsumeEvents()
 }
--- internal/monitor/monitor.go
+++ internal/monitor/monitor.go
@@ -1,1 +1,65 @@
 package monitor
+
+import (
+	"context"
+	"log"
+	"sync"
+
+	"github.com/segmentio/kafka-go"
+)
+
+type MonitorServer struct {
+	KafkaConsumer *kafka.Reader
+}
+
+func (s *MonitorServer) ConsumeEvents() {
+	log.Printf("Starting monitor service...")
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	defer s.KafkaConsumer.Close()
+
+	const (
+		workers      = 8
+		channelDepth = 32 // Managing backpressure
+	)
+	var wg sync.WaitGroup
+	msgChan := make(chan kafka.Message, channelDepth)
+
+	for i := 0; i < workers; i++ {
+		wg.Add(1)
+
+		go func(workerId int) {
+			defer wg.Done()
+			for m := range msgChan {
+				s.processEvents(m, workerId)
+
+				if err := s.KafkaConsumer.CommitMessages(ctx, m); err != nil {
+					log.Printf("Error commting message: %v", err)
+				}
+			}
+		}(i)
+	}
+
+	for {
+		m, err := s.KafkaConsumer.FetchMessage(ctx)
+		if err != nil {
+			if ctx.Err() != nil {
+				log.Printf("Context error: %v", ctx.Err())
+				break
+			}
+
+			log.Printf("Error fetching message: %v", err)
+			continue
+		}
+
+		msgChan <- m
+	}
+
+	close(msgChan)
+	wg.Wait()
+	log.Printf("shutting down...")
+}
+
+func (s *MonitorServer) processEvents(m kafka.Message, workerId int) {
+	log.Printf("processing events from worker %d: %v", workerId, m)
+}
